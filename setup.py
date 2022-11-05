5aa8cb63d358b2bb20538fd8d4466922a845fdfd
Viktor Persson <vph.persson@gmail.com>
2022-11-05 20:55:58 +0100

    Fix optional pattern.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ecs_tools_py',
-    version='0.86',
+    version='0.87',
     packages=find_packages(),
     install_requires=[
         'ecs_py @ git+https://github.com/vphpersson/ecs_py.git#egg=ecs_py',
